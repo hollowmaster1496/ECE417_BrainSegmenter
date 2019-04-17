ee01cab349dec7917d55b478b1f854a1a1086b71
shaseeb <haseeb1496@gmail.com>
2019-04-17 18:05:30 -0400

    main.m: Reformatted outline with all necessary sections of final report

--- src/main.m
+++ src/main.m
@@ -1,5 +1,69 @@
-# Paper: MRI Fuzzy Segmentation of Brain TIssue using Neighborhood Extraction
-# Author: Shan Shen, William Sandham, etc.
-
-# ECE417 - Image Processing 
-
+%% MRI Fuzzy Segmentation of Brain using Neighborhood Extraction
+%
+% Paper Authors: Shan Shen, William Sandham, et al.
+%
+% Implementation by: Haseeb, Bhavishey, Dishant
+%
+%
+%% 1) Formulation
+% 
+%     TODO: Add clear description of problem to be solved
+%
+% 
+% 
+%
+%
+%% 2) Proposed Solution
+% 
+%     TODO: Add overall algorithmic workflow of proposed solution
+%
+% 
+% 
+%
+%
+%% 3) Data Sources
+% 
+%     TODO: Add overall algorithmic workflow of proposed solution
+%
+% 
+% 
+%
+%
+%% 4) Solution
+% 
+%     TODO: Necessary details on structure of algorithm 
+%     TODO: Add actual implementation code
+%
+% 
+% 
+%
+%
+%% 5) Visualization of Results
+% 
+%     TODO: Plot images here
+%
+% 
+% 
+%
+%
+%% 6) Analysis and Conclusion
+% 
+%     TODO: Answer some questions
+%           1) Have you been able to reproduce results reported in paper?
+%           
+%           2) Did algorithm behave in a predictable way (as described by authors)?
+%
+%           3) Do your own conclusions support those made by the authors?
+%
+%           4) What are the drawbacks (if any) of the proposed solution? 
+% 
+%
+%
+%% 7) Custom source files
+% 
+%     N/A
+%
+% 
+% 
+%
+%
